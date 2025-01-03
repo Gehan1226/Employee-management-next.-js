20dea46f6c65c576d3f6ee12754e6e1c0778b3a6
Gehan1226 <gehansithija2023@gmail.com>
2025-01-03 07:55:29 +0530

    Refactor Input component styles; simplify class names for improved readability and maintainability

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -13,23 +13,13 @@ export default function Input({ label, id, error, ...props }: Readonly<InputProp
             <input
                 type="text"
                 id={id}
-                className={clsx("block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer",
-                    {
-                        "border-gray-300 focus:border-blue-600": !error,
-                        "border-red-400 focus:border-red-600": error
-                    }
-                )}
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 peer border-gray-300 focus:border-blue-600"
                 placeholder=" "
                 {...props}
             />
             <label
                 htmlFor={id}
-                className={clsx("peer-focus:font-medium absolute text-sm duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6",
-                    {
-                        "text-gray-500 peer-focus:text-blue-600": !error,
-                        "text-red-400 peer-focus:text-red-500": error
-                    }
-                )}
+                className="peer-focus:font-medium absolute text-sm duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-gray-500 peer-focus:text-blue-600"
             >
                 {label}
             </label>
