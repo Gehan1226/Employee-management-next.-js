56af1d7fa7fda5c4a629656ec362e29c693cb8d1
Gehan1226 <gehansithija2023@gmail.com>
2025-01-05 09:47:40 +0530

    Add UserRequest component and update user requests page layout; enhance UI for pending user requests

--- src/app/admin/user-requests/page.tsx
+++ src/app/admin/user-requests/page.tsx
@@ -1,7 +1,17 @@
+import UserRequest from '@/app/components/admin/UserRequest'
+import Image from 'next/image'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <>
+
+      <div className=' bg-blue-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 shadow-md'>
+        <p className='text-center p-5 font-semibold text-2xl'>Pending User Requests</p>
+      </div>
+
+      <UserRequest />
+    </>
+
   )
 }
---
+++ src/app/components/admin/UserRequest.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function UserRequest() {
+    return (
+        <div className="mt-5 w-full p-4 bg-blue-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0 rounded-lg shadow-lg ">
+
+            <div className='flex flex-col justify-between sm:flex-row gap-5'>
+                <div className='flex flex-col items-center sm:flex-row'>
+                    <Image
+                        className="w-16 h-16 rounded-full mt-2"
+                        src="/user-placeholder.png"
+                        alt="user placeholder image"
+                        width={64}
+                        height={64}
+                    />
+
+
+                    <div className="flex-1 min-w-0 ms-4">
+                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            Example user
+                        </p>
+                        <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                            example@gmail.com
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className='flex items-center font-semibold text-center'>
+                    <p>Request at: {new Date().toDateString()} </p>
+                </div>
+
+                <div className='flex flex-row justify-center gap-3 sm:items-center'>
+                    <button
+                        type="button"
+                        className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5"
+                    >
+                        Accept
+                    </button>
+
+                    <button
+                        type="button"
+                        className="text-white bg-red-500 hover:bg-red-600 font-medium rounded-lg text-sm px-5 py-2.5"
+                    >
+                        Delete
+                    </button>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
