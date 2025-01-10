5a27b98c1871c0680d39e2f9d721e18468be8b8f
Gehan1226 <gehansithija2023@gmail.com>
2025-01-10 09:22:05 +0530

    Fetch and display disabled users in UserRequest component; update layout to include search and filter options

--- src/app/admin/user-requests/page.tsx
+++ src/app/admin/user-requests/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+import { getDisabledUsers } from '@/app/api/auth';
 import UserRequest from '@/app/components/admin/UserRequest'
 import SearchBar from '@/app/components/SearchBar'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function page() {
+  const [disabledUsers, setDisabledUsers] = useState<BasicUserInfo[]>([]);
+
+  useEffect(() => {
+    const fetchDisabledUsers = async () => {
+      const response = await getDisabledUsers();
+      if (!response.message) {
+        setDisabledUsers(response.data || []);
+      }
+    };
+    fetchDisabledUsers();
+  }, []);
+
   return (
     <>
 
@@ -10,34 +24,73 @@ export default function page() {
         <p className='text-center p-5 font-semibold text-2xl mb-5'>Pending User Requests</p>
       </div>
 
-      <div className="grid grid-cols-6 gap-4">
-        <div className="col-span-4">
-          <SearchBar />
-        </div>
+      {disabledUsers.length > 0 ? (
+        <>
+          <div className="grid grid-cols-6 gap-4">
+            <div className="col-span-4">
+              <SearchBar />
+            </div>
 
-        <button id="sortDropdownButton1" data-dropdown-toggle="dropdownSort1" type="button" className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 sm:w-auto">
-          <svg className="-ms-0.5 me-2 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 4v16M7 4l3 3M7 4 4 7m9-3h6l-6 6h6m-6.5 10 3.5-7 3.5 7M14 18h4" />
-          </svg>
-          Sort
-        </button>
+            <button
+              id="sortDropdownButton1"
+              data-dropdown-toggle="dropdownSort1"
+              type="button"
+              className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 sm:w-auto"
+            >
+              <svg
+                className="-ms-0.5 me-2 h-4 w-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M7 4v16M7 4l3 3M7 4 4 7m9-3h6l-6 6h6m-6.5 10 3.5-7 3.5 7M14 18h4"
+                />
+              </svg>
+              Sort
+            </button>
 
-        <button
-          data-modal-toggle="filterModal"
-          data-modal-target="filterModal"
-          type="button"
-          className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 sm:w-auto"
-        >
-          <svg className="-ms-0.5 me-2 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z" />
-          </svg>
-          Filters
-        </button>
+            <button
+              data-modal-toggle="filterModal"
+              data-modal-target="filterModal"
+              type="button"
+              className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 sm:w-auto"
+            >
+              <svg
+                className="-ms-0.5 me-2 h-4 w-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"
+                />
+              </svg>
+              Filters
+            </button>
+          </div>
+          
+          <UserRequest disabledUsers={disabledUsers} />
+        </>
+      ) : (
+        <p className="text-center p-5 font-semibold text-2xl mt-5">
+          Not Pending User request found....
+        </p>
+      )}
 
-      </div>
-
-      <UserRequest />
     </>
-
   )
 }
--- src/app/components/admin/UserRequest.tsx
+++ src/app/components/admin/UserRequest.tsx
@@ -1,24 +1,15 @@
-"use client";
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import RequestCard from './RequestCard'
 import ManageUserRequestPopup from './ManageUserRequestPopup';
-import { getDisabledUsers } from '@/app/api/auth';
 
-export default function UserRequest() {
+type UserRequestProps = {
+    disabledUsers: BasicUserInfo[];
+}
 
-    const [isShowUserRequestPopup, setIsShowUserRequestPopup] = useState(false);
-    const [disabledUsers, setDisabledUsers] = useState<BasicUserInfo[]>([]);
+export default function UserRequest({ disabledUsers }: Readonly<UserRequestProps>) {
     const [selectedUser, setSelectedUser] = useState<BasicUserInfo>();
+    const [isShowUserRequestPopup, setIsShowUserRequestPopup] = useState(false);
 
-    useEffect(() => {
-        const fetchDisabledUsers = async () => {
-            const response = await getDisabledUsers();
-            if (!response.message) {
-                setDisabledUsers(response.data || []);
-            }
-        };
-        fetchDisabledUsers();
-    }, []);
 
     const handleUserRequestPopup = (value: boolean) => {
         setIsShowUserRequestPopup(value);
