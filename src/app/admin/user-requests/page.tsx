7fb2aeecf52f321d0bad53c223e0773e71f20b0a
Gehan1226 <gehansithija2023@gmail.com>
2025-01-10 17:46:15 +0530

    Add sorting and filtering functionality to user requests page; include new icons for better UI

--- src/app/admin/user-requests/page.tsx
+++ src/app/admin/user-requests/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { getDisabledUsers } from '@/app/api/auth';
 import UserRequest from '@/app/components/admin/UserRequest'
+import FilterIcon from '@/app/components/icons/FilterIcon';
+import SortIcon from '@/app/components/icons/SortIcon';
 import SearchBar from '@/app/components/SearchBar'
 import React, { useEffect, useState } from 'react'
 
@@ -32,57 +34,22 @@ export default function page() {
             </div>
 
             <button
-              id="sortDropdownButton1"
-              data-dropdown-toggle="dropdownSort1"
               type="button"
-              className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 sm:w-auto"
+              className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none sm:w-auto shadow-md"
             >
-              <svg
-                className="-ms-0.5 me-2 h-4 w-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M7 4v16M7 4l3 3M7 4 4 7m9-3h6l-6 6h6m-6.5 10 3.5-7 3.5 7M14 18h4"
-                />
-              </svg>
+              <SortIcon />
               Sort
             </button>
 
             <button
-              data-modal-toggle="filterModal"
-              data-modal-target="filterModal"
               type="button"
-              className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 sm:w-auto"
+              className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none sm:w-auto shadow-md"
             >
-              <svg
-                className="-ms-0.5 me-2 h-4 w-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeWidth="2"
-                  d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"
-                />
-              </svg>
+              <FilterIcon />
               Filters
             </button>
           </div>
-          
+
           <UserRequest disabledUsers={disabledUsers} />
         </>
       ) : (
--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -11,7 +11,7 @@ export default function SearchBar() {
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                     </svg>
                 </div>
-                <input type="search" id="default-search" className="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Search Employees..." required />
+                <input type="search" id="default-search" className="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 shadow-md " placeholder="Search Employees..." required />
             </div>
         </form>
     )
