36e14fb4582ae67332769facf5bd8bdd1612f6ea
Gehan1226 <gehansithija2023@gmail.com>
2025-01-22 14:09:29 +0530

    Refactor AdminUserRequestPage layout for improved readability and user experience

--- src/app/components/pages/AdminUserRequestPage.tsx
+++ src/app/components/pages/AdminUserRequestPage.tsx
@@ -102,58 +102,67 @@ export default function AdminUserRequestPage() {
         </p>
       </div>
 
-      {disabledUsers.length > 0 ? (
-        <>
-          <div className="grid grid-cols-6 gap-4">
-            <div className="col-span-4">
-              <SearchBar
-                placeholder="Search Users..."
-                onSearch={handleSearch}
-              />
-            </div>
-
-            <button
-              type="button"
-              className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none sm:w-auto shadow-md"
-            >
-              <SortIcon />
-              Sort
-            </button>
-
-            <button
-              type="button"
-              className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none sm:w-auto shadow-md"
-              onClick={() => setIsOpenUserFilterPopup(true)}
-            >
-              <FilterIcon />
-              Filters
-            </button>
+      <>
+        <div className="grid grid-cols-6 gap-4">
+          <div className="col-span-4">
+            <SearchBar placeholder="Search Users..." onSearch={handleSearch} />
           </div>
 
-          <div className="flex mt-4 ml-4">
+          <button
+            type="button"
+            className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none sm:w-auto shadow-md"
+          >
+            <SortIcon />
+            Sort
+          </button>
+
+          <button
+            type="button"
+            className="flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none sm:w-auto shadow-md"
+            onClick={() => setIsOpenUserFilterPopup(true)}
+          >
+            <FilterIcon />
+            Filters
+          </button>
+        </div>
+
+        <div className="flex mt-4 ml-4 gap-4">
+          <p className="font-semibold bg-gray-300 px-5 py-1 rounded-sm shadow-sm relative">
+            <NotificationIcon className="absolute -top-1 right-0 w-4 h-4" />
+            Total requests: {paginationData.totalElements}
+          </p>
+
+          {filterData.startDate && (
             <p className="font-semibold bg-gray-300 px-5 py-1 rounded-sm shadow-sm relative">
-              <NotificationIcon className="absolute -top-1 right-0 w-4 h-4" />
-              Total requests: {paginationData.totalElements}
+              Filters Applied
             </p>
-          </div>
+          )}
+        </div>
 
-          <UserRequest
-            disabledUsers={disabledUsers}
-            handleDeleteUser={onDeleteUser}
-          />
+        {disabledUsers.length > 0 ? (
+          <>
+            <UserRequest
+              disabledUsers={disabledUsers}
+              handleDeleteUser={onDeleteUser}
+            />
 
-          <Paginations
-            currentPage={paginationData.currentPage}
-            totalPages={paginationData.totalPages}
-            handlePrev={() => handlePageChange(paginationData.currentPage - 1)}
-            handleNext={() => handlePageChange(paginationData.currentPage + 1)}
-          />
-        </>
-      ) : (
-        <p className="text-center p-5 font-semibold text-2xl mt-5">
-          Not Pending User request found....
-        </p>
-      )}
+            <Paginations
+              currentPage={paginationData.currentPage}
+              totalPages={paginationData.totalPages}
+              handlePrev={() =>
+                handlePageChange(paginationData.currentPage - 1)
+              }
+              handleNext={() =>
+                handlePageChange(paginationData.currentPage + 1)
+              }
+            />
+          </>
+        ) : (
+          <p className="text-center p-5 font-semibold text-2xl mt-5">
+            No pending user requests found....
+          </p>
+        )}
+      </>
     </>
   );
 }
