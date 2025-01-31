35556f9059429f94d81fe5e91ed7488f395ebf9c
Gehan1226 <gehansithija2023@gmail.com>
2025-01-31 18:44:22 +0530

    Update AddDepartmentModal with new title, improved layout, and updated form elements

--- src/app/components/admin/AddDepartmentModal.tsx
+++ src/app/components/admin/AddDepartmentModal.tsx
@@ -24,39 +24,31 @@ export default function AddDepartmentModal() {
           Add Department
         </button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[800px] w-full">
+      <DialogContent className="sm:max-w-[800px] w-5/6">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Create new department</DialogTitle>
           <DialogDescription>
             Make changes to your profile here. Click save when you're done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <form action="" className="flex flex-col gap-3">
+          <form action="" className="flex flex-col gap-3 px-4">
             <Input label="Department name" id="dep-name" />
-            <Textarea placeholder="Department description" id="dep-desc" />
+            <Textarea placeholder="Department resposibilities" id="dep-desc" />
 
-            <DropDownMenu
-              label="Gender"
-              menuItems={[
-                { label: "Male", id: "Male" },
-                { label: "Female", id: "Female" },
-              ]}
-              name="gender"
-            />
-
-            <DropDownMenu
-              label="Gender"
-              menuItems={[
-                { label: "Male", id: "Male" },
-                { label: "Female", id: "Female" },
-              ]}
-              name="gender"
-            />
+            <select name="" id="" className="mt-4">
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
           </form>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 shadow-md"
+          >
+            Save
+          </button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
