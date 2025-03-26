fabb06aaf0b41cb0e28fe1ad8dd320da80ac2f8c
Gehan Sithija <157275598+Gehan1226@users.noreply.github.com>
2025-03-26 12:48:14 +0530

    Add multi-step form for employee addition: implement AddEmployeeStepper and refactor AddEmployeeForm for step-based navigation

--- src/components/employee/AddEmployeeForm.tsx
+++ src/components/employee/AddEmployeeForm.tsx
@@ -5,8 +5,58 @@ import CountrySelector from "../CountrySelector";
 import Input from "../Input";
 import { Card, CardContent } from "../card";
 import Image from "next/image";
+import AddEmployeeStepper from "../manager/AddEmployeeStepper";
+import { useState } from "react";
+
+const steps = [
+  "Select campaign settings",
+  "Create an ad group",
+  "Create an ad",
+];
 
 export default function AddEmployeeForm() {
+  const [step, setStep] = useState<"STEP1" | "STEP2">("STEP1");
+
+  const [activeStep, setActiveStep] = useState(0);
+  const [completed, setCompleted] = useState<{
+    [k: number]: boolean;
+  }>({});
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    setStep("STEP2");
+    setCompleted({
+      ...completed,
+      [activeStep]: true,
+    });
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const handleStep = (step: number) => () => {
+    setActiveStep(step);
+  };
+
   return (
     <div className="px-16 py-5 overflow-hidden">
       <Card className="py-5">
@@ -16,60 +66,72 @@ export default function AddEmployeeForm() {
 
         <CardContent>
           <div className="relative">
-            <form className="px-20 mt-4">
-              <div className="grid md:grid-cols-2 md:gap-6">
-                <Input
-                  type="text"
-                  label="First Name"
-                  id="first_name"
-                  name="firstName"
-                  placeholder="john"
-                />
-                <Input
-                  type="text"
-                  label="Last Name"
-                  id="last_name"
-                  name="lastName"
-                  placeholder="doe"
-                />
-              </div>
+            <div className="w-3/4 mx-auto mt-10">
+              <AddEmployeeStepper
+                steps={steps}
+                activeStep={activeStep}
+                completed={completed}
+                handleStep={handleStep}
+              />
 
-              <div className="">
-                <Input
-                  type="text"
-                  label="Email address"
-                  id="floating_email"
-                  name="email"
-                  placeholder="joe@doe.com"
-                />
-              </div>
+            </div>
 
-              <div className="mb-6 ">
-                <Input
-                  type="tel"
-                  label="Mobile number"
-                  id="mobile"
-                  name="phoneNumber"
-                  placeholder="1234567890"
-                />
-              </div>
+            <form className="px-20 mt-10">
+              {step === "STEP1" && (
+                <>
+                  <div className="grid md:grid-cols-2 md:gap-6">
+                    <Input
+                      type="text"
+                      label="First Name"
+                      id="first_name"
+                      name="firstName"
+                      placeholder="john"
+                    />
+                    <Input
+                      type="text"
+                      label="Last Name"
+                      id="last_name"
+                      name="lastName"
+                      placeholder="doe"
+                    />
+                  </div>
 
-              <div className="mb-6 ">
-                <DateInput label="Date of birth" name="dob" />
-              </div>
+                  <div className="">
+                    <Input
+                      type="text"
+                      label="Email address"
+                      id="floating_email"
+                      name="email"
+                      placeholder="joe@doe.com"
+                    />
+                  </div>
 
-              <div className="grid md:grid-cols-2 gap-6">
-                <DropDownMenu
-                  label="Gender"
-                  menuItems={[
-                    { label: "Male", id: "Male" },
-                    { label: "Female", id: "Female" },
-                    { label: "Other", id: "Other" },
-                  ]}
-                  name="gender"
-                />
+                  <div className="mb-6 ">
+                    <Input
+                      type="tel"
+                      label="Mobile number"
+                      id="mobile"
+                      name="phoneNumber"
+                      placeholder="1234567890"
+                    />
+                  </div>
 
-                {/* <DropDownMenu
+                  <div className="mb-6 ">
+                    <DateInput label="Date of birth" name="dob" />
+                  </div>
+
+                  <div className="grid md:grid-cols-2 gap-6">
+                    <DropDownMenu
+                      label="Gender"
+                      menuItems={[
+                        { label: "Male", id: "Male" },
+                        { label: "Female", id: "Female" },
+                        { label: "Other", id: "Other" },
+                      ]}
+                      name="gender"
+                    />
+
+                    {/* <DropDownMenu
                             label="Department"
                             menuItems={mapDepartmentToDropdownItem(departments)}
                             name='department'
@@ -83,56 +145,74 @@ export default function AddEmployeeForm() {
                             name='role'
                             error={state.validationErrors?.role}
                         /> */}
-              </div>
+                  </div>
 
-              <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+                  <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+                </>
+              )}
 
-              {/* <CountrySelector name="country" />
+              {step === "STEP2" && (
+                <>
+                  <CountrySelector name="country" />
 
-              <div className="grid md:grid-cols-2 gap-3 mt-6">
-                <Input
-                  type="text"
-                  label="State"
-                  id="floating_state"
-                  name="state"
-                />
+                  <div className="grid md:grid-cols-2 gap-3 mt-6">
+                    <Input
+                      type="text"
+                      label="State"
+                      id="floating_state"
+                      name="state"
+                    />
 
-                <Input
-                  type="text"
-                  label="District"
-                  id="floating_district"
-                  name="district"
-                />
+                    <Input
+                      type="text"
+                      label="District"
+                      id="floating_district"
+                      name="district"
+                    />
 
-                <Input
-                  type="text"
-                  label="City"
-                  id="floating_city"
-                  name="city"
-                />
+                    <Input
+                      type="text"
+                      label="City"
+                      id="floating_city"
+                      name="city"
+                    />
 
-                <Input
-                  type="text"
-                  label="Street"
-                  id="floating_street"
-                  name="street"
-                />
+                    <Input
+                      type="text"
+                      label="Street"
+                      id="floating_street"
+                      name="street"
+                    />
 
-                <Input
-                  type="text"
-                  label="Postal code"
-                  id="floating_postal"
-                  name="postalCode"
-                />
-              </div> */}
+                    <Input
+                      type="text"
+                      label="Postal code"
+                      id="floating_postal"
+                      name="postalCode"
+                    />
+                  </div>
+                </>
+              )}
 
               <div className="flex flex-row-reverse mt-6">
-                <button
-                  type="submit"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-                >
-                  Submit
-                </button>
+                {step === "STEP2" && (
+                  <button
+                    type="submit"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                  >
+                    Submit
+                  </button>
+                )}
+
+                {step === "STEP1" && (
+                  <button
+                    type="button"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    onClick={handleNext}
+                  >
+                    Next
+                  </button>
+                )}
               </div>
             </form>
 
---
+++ src/components/manager/AddEmployeeStepper.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepButton from "@mui/material/StepButton";
+
+type AddEmployeeStepperProps = {
+  steps: string[];
+  activeStep: number;
+  completed: { [key: number]: boolean };
+  handleStep: (step: number) => () => void;
+};
+
+export default function AddEmployeeStepper({ steps, activeStep, completed, handleStep }: Readonly<AddEmployeeStepperProps>) {
+
+  
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stepper nonLinear activeStep={activeStep} alternativeLabel>
+        {steps.map((label, index) => (
+          <Step key={label} completed={completed[index]}>
+            <StepButton color="inherit" onClick={handleStep(index)}>
+              {label}
+            </StepButton>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+}
