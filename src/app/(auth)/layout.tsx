71f940df33936348e12562242f762a58f13e94e3
Gehan Sithija <157275598+Gehan1226@users.noreply.github.com>
2025-04-01 22:39:46 +0530

    Add account selection page with user role options and update layout component to use Readonly props

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <div className="h-screen w-full bg-gradient-to-r from-sky-700 via-sky-600 to-sky-800 flex flex-col md:flex-row gap-5">
       {children}
