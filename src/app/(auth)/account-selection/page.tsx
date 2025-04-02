535b1ee0efb7ca6f75d64b93198c9a04016a8021
Gehan Sithija <157275598+Gehan1226@users.noreply.github.com>
2025-04-02 09:29:55 +0530

    Add account components for Admin, Employee, and Manager to account selection page

--- src/app/(auth)/account-selection/page.tsx
+++ src/app/(auth)/account-selection/page.tsx
@@ -1,5 +1,6 @@
-import { Divider } from "@mui/material";
-import Image from "next/image";
+import AdminAccount from "@/components/account/AdminAccount";
+import EmployeeAccount from "@/components/account/EmployeeAccount";
+import ManagerAccount from "@/components/account/ManagerAccount";
 import React from "react";
 
 export default function Page() {
@@ -11,80 +12,9 @@ export default function Page() {
             Select an Account
           </p>
 
-          <div className="bg-violet-100 flex justify-center items-center gap-10 px-3 py-3 rounded-md shadow-md mt-5">
-            <div className="flex gap-4 justify-center items-center">
-              <div className="flex flex-col justify-center items-center">
-                <Image
-                  src="/employee-type-img.png"
-                  alt="employee image"
-                  width={50}
-                  height={50}
-                />
-                <p className="font-semibold text-sm text-gray-600">Employee</p>
-              </div>
-              <Divider orientation="vertical" variant="middle" flexItem />
-              <div className="flex flex-col">
-                <p className="font-semibold ">Gehan Sithija</p>
-                <p className="font-semibold text-xs text-slate-500">
-                  gehansithija1226@gmail.com
-                </p>
-              </div>
-            </div>
-
-            <button className="bg-indigo-400 hover:bg-indigo-500 rounded-md px-7 py-2 h-fit text-white">
-              Select
-            </button>
-          </div>
-
-          <div className="bg-green-100 flex justify-center items-center gap-10 px-3 py-3 rounded-md shadow-md mt-2">
-            <div className="flex gap-4 justify-center items-center">
-              <div className="flex flex-col justify-center items-center">
-                <Image
-                  src="/manager-type-img.png"
-                  alt="employee image"
-                  width={50}
-                  height={50}
-                />
-                <p className="font-semibold text-sm text-gray-600">Manager</p>
-              </div>
-              <Divider orientation="vertical" variant="middle" flexItem />
-              <div className="flex flex-col">
-                <p className="font-semibold ">Gehan Sithija</p>
-                <p className="font-semibold text-xs text-slate-500">
-                  gehansithija1226@gmail.com
-                </p>
-              </div>
-            </div>
-
-            <button className="bg-emerald-400 hover:bg-emerald-500 rounded-md px-7 py-2 h-fit text-white">
-              Select
-            </button>
-          </div>
-
-          <div className="bg-red-100 flex justify-center items-center gap-10 px-3 py-3 rounded-md shadow-md mt-2">
-            <div className="flex gap-4 justify-center items-center">
-              <div className="flex flex-col justify-center items-center">
-                <Image
-                  src="/admin-type-img.png"
-                  alt="employee image"
-                  width={50}
-                  height={50}
-                />
-                <p className="font-semibold text-sm text-gray-600">Admin</p>
-              </div>
-              <Divider orientation="vertical" variant="middle" flexItem />
-              <div className="flex flex-col">
-                <p className="font-semibold ">Gehan </p>
-                <p className="font-semibold text-xs text-slate-500">
-                  gehansithija1226@gmail.com
-                </p>
-              </div>
-            </div>
-
-            <button className="bg-orange-400 hover:bg-orange-500 rounded-md px-7 py-2 h-fit text-white">
-              Select
-            </button>
-          </div>
+          <EmployeeAccount />
+          <ManagerAccount />
+          <AdminAccount />
         </div>
       </div>
     </div>
---
+++ src/components/account/AdminAccount.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Divider } from "@mui/material";
+import Image from "next/image";
+
+export default function AdminAccount() {
+  return (
+    <div className="bg-red-100 flex justify-center items-center gap-10 px-3 py-3 rounded-md shadow-md mt-2">
+      <div className="flex gap-4 justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          <Image
+            src="/admin-type-img.png"
+            alt="employee image"
+            width={50}
+            height={50}
+          />
+          <p className="font-semibold text-sm text-gray-600">Admin</p>
+        </div>
+        <Divider orientation="vertical" variant="middle" flexItem />
+        <div className="flex flex-col">
+          <p className="font-semibold ">Gehan </p>
+          <p className="font-semibold text-xs text-slate-500">
+            gehansithija1226@gmail.com
+          </p>
+        </div>
+      </div>
+
+      <button className="bg-orange-400 hover:bg-orange-500 rounded-md px-7 py-2 h-fit text-white">
+        Select
+      </button>
+    </div>
+  );
+}
