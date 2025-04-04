a40fd8bc72da6115b04a6d5f006cab561902153a
Gehan Sithija <157275598+Gehan1226@users.noreply.github.com>
2025-04-04 22:44:23 +0530

    Add logout confirmation dialog to UserProfileButton, implement logout handling, and update account selection link

--- src/components/user/UserProfileButton.tsx
+++ src/components/user/UserProfileButton.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 import Menu from "@mui/material/Menu";
 import { UserResponse } from "@/types/auth-types";
 import { ArrowLeft, User, LogOut } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { clearAuthCookie } from "@/lib/util/cookie";
+import ConfirmDialog from "../ConfirmDialog";
 
 type UserProfileButtonProps = {
   user: UserResponse | undefined;
@@ -11,7 +15,10 @@ type UserProfileButtonProps = {
 export default function UserProfileButton({
   user,
 }: Readonly<UserProfileButtonProps>) {
+  const [openAlert, setOpenAlert] = useState(false);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const router = useRouter();
+
   const open = Boolean(anchorEl);
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -20,6 +27,17 @@ export default function UserProfileButton({
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    setAnchorEl(null);
+    setOpenAlert(true);
+  };
+
+  const handleConfirmLogout = async () => {
+    setOpenAlert(false);
+    await clearAuthCookie();
+    router.push("/user-login");
   };
 
   return (
@@ -64,15 +82,28 @@ export default function UserProfileButton({
           <User width={15} height={15} />
           User Profile
         </button>
-        <button className="flex gap-2 items-center hover:bg-gray-200 px-5 py-2 w-full rounded-lg">
+        <Link
+          href="/account-selection"
+          className="flex gap-2 items-center hover:bg-gray-200 px-5 py-2 w-full rounded-lg"
+        >
           <ArrowLeft width={15} height={15} />
           Account selection
-        </button>
-        <button className="flex gap-2 items-center hover:bg-gray-200 px-5 py-2 w-full rounded-lg">
+        </Link>
+        <button
+          className="flex gap-2 items-center hover:bg-gray-200 px-5 py-2 w-full rounded-lg"
+          onClick={handleLogout}
+        >
           <LogOut width={15} height={15} />
           Log out
         </button>
       </Menu>
+
+      <ConfirmDialog
+        open={openAlert}
+        handleClose={() => setOpenAlert(false)}
+        handleConfirm={handleConfirmLogout}
+        message="Are you sure to logout?. You will be redirected to login page."
+      />
     </>
   );
 }
